1393194b04a6c241cfdf977300fef4b9b101fd56
Shania Faith Ramos <117431758+shaniassi@users.noreply.github.com>
2024-07-08 03:26:48 +0800

    edited Organization form layout

--- bayanijuan/src/components/Register/Organization.jsx
+++ bayanijuan/src/components/Register/Organization.jsx
@@ -7,77 +7,94 @@ const Organization = () => {
       <div className="mt-20 min-w-50 bg-sun shadow-md rounded-lg p-8">
         <h2 className="text-7xl font-medium mb-6 font-header text-center text-blue">Create an Organization Account</h2>
         <form>
-            <div class="mb-5">
-                <label for="organization name" class="mb-3 block text-base font-medium text-[#07074D]">
-                    Organization Name
-                </label>
-                <input type="text" name="organization name" id="email" placeholder="e.g. Your Company Name"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-            </div>
-              <div class="mb-5">
-                  <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">
-                      Email Address
-                  </label>
-                  <input type="email" name="email" id="email" placeholder="yourcompany@example.com"
-                      class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-              </div>
-              
-              
-
-              <div class="mb-5">
-                <label for="password" class="mb-3 block text-base font-medium text-[#07074D]">
-                    Password
-                </label>
-                <input type="password" name="email" id="email" placeholder="Enter your password"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-            </div>
-
-            <div class="mb-5">
-              <label for="password" class="mb-3 block text-base font-medium text-[#07074D]">
-                  Confirm Password
-              </label>
-              <input type="password" name="email" id="email" placeholder="Confirm your password"
-                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+          <div className="mb-5">
+            <label htmlFor="organizationName" className="mb-3 block text-base font-medium text-[#07074D]">
+              Organization Name
+            </label>
+            <input
+              type="text"
+              id="organizationName"
+              name="organizationName"
+              placeholder="e.g. Your Company Name"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
           </div>
 
-          <div class="-mx-3 flex flex-wrap">
-            <div class="w-full px-3 sm:w-1/2">
-                <div class="mb-5">
-                    <label for="contact person name" class="mb-3 block text-base font-medium text-[#07074D]">
-                        Contact Person Name
-                    </label>
-                    <input type="text" name="contact person name" id="date" placeholder="e.g. John"
-                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                </div>
-            </div>
-            <div class="w-full px-3 sm:w-1/2">
-                <div class="mb-5">
-                    <label for="contact person title" class="mb-3 block text-base font-medium text-[#07074D]">
-                        Contact Person Title
-                    </label>
-                    <input type="text" name="contact person title" id="time" placeholder="e.g. Doe"
-                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                </div>
-            </div>
-            </div>
-            <div class="mb-5">
-                <label for="phone" class="mb-3 block text-base font-medium text-[#07074D]">
-                    Phone Number
-                </label>
-                <input type="text" name="phone" id="phone" placeholder="Enter your phone number"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-            </div>
-              
-              
-                <div>
-                  <button
-                      class="hover:shadow-form w-full rounded-md bg-blue py-3 px-8 text-center text-base font-semibold text-white outline-none">
-                      Next
-                  </button>
-              </div>
-          </form>
+          <div className="mb-5">
+            <label htmlFor="email" className="mb-3 block text-base font-medium text-[#07074D]">
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="yourcompany@example.com"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="contactPersonName" className="mb-3 block text-base font-medium text-[#07074D]">
+              Contact Person Name
+            </label>
+            <input
+              type="text"
+              id="contactPersonName"
+              name="contactPersonName"
+              placeholder="e.g. John"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="contactPersonTitle" className="mb-3 block text-base font-medium text-[#07074D]">
+              Contact Person Title
+            </label>
+            <input
+              type="text"
+              id="contactPersonTitle"
+              name="contactPersonTitle"
+              placeholder="e.g. Doe"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="phone" className="mb-3 block text-base font-medium text-[#07074D]">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              placeholder="Enter your phone number"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="password" className="mb-3 block text-base font-medium text-[#07074D]">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+
+          <div>
+            <button className="hover:shadow-form w-full rounded-md bg-blue py-3 px-8 text-center text-base font-semibold text-white outline-none">
+              Submit
+            </button>
+          </div>
+        </form>
         <div className="mt-4 text-center">
-          <p>Already have an account? <Link to="/login" className="text-blue font-bold">Login</Link></p>
+          <p>
+            Already have an account? <Link to="/login" className="text-blue font-bold">Login</Link>
+          </p>
         </div>
       </div>
     </div>
