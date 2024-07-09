0ab58d3fc923e5f972b31748e50f1f7b8072786e
Leina Gray <abad.alleina@gmail.com>
2024-07-09 12:33:39 +0800

    Add description and image in Register Page

--- bayanijuan/src/components/Register/Register1.jsx
+++ bayanijuan/src/components/Register/Register1.jsx
@@ -10,19 +10,22 @@ const Register1 = () => {
       {/* First Section */}
       <div className="w-full h-96 bg-sun mt-36 flex items-center justify-center mb-5">
         <div className="w-[300px] flex items-center justify-center">
-          <img src={placeholderImage} alt="Placeholder" className="h-64 w-72 object-cover rounded-lg shadow-lg border-2 border-blue-500" />
+          <img src="https://images.summitmedia-digital.com/spotph/images/places-to-volunteer_642.jpg" alt="Placeholder" className="h-64 w-72 object-cover rounded-lg shadow-lg border-2 border-blue-500" />
         </div>
         {/* Right side: Text content */}
         <div className="w-[700px] p-8">
-          <h1 className="text-9xl text-blue mb-0 font-header font-medium">Register Now</h1>
-          <p className="text-lg text-gray-700 mb-10">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <h1 className="text-9xl text-blue mb-0 font-header font-medium">Be a Hero!</h1>
+
+
+          <p className="text-lg text-gray-700 mb-10">Want to Volunteer? Find meaningful volunteer opportunities that align with your skills and interests. 
+            Contribute your unique talents and make a real impact in your community. Be part of a network of passionate individuals 
+            creating positive change in the Philippines.</p>
+          
         </div>
         <div className='mt-60'>
           <Link to="/Volunteer">
             <button className="bg-blue text-cream py-2 px-10 rounded-full font-semibold hover:bg-rose-dark transition duration-300">
-              Join as Volunteer
+              Register as Volunteer
             </button>
           </Link>
         </div>
@@ -32,10 +35,10 @@ const Register1 = () => {
       <div className="w-full h-96 flex items-center justify-center">
         <div className="w-[800px] items-center justify-center">
           <div className="text-right">
-            <h2 className="text-9xl text-blue mb-0 font-header font-medium">Third Section</h2>
+            <h2 className="text-9xl text-blue mb-0 font-header font-medium">Empower Your Cause!</h2>
             <p className="text-lg text-gray-700 mb-10">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
+            Want to find volunteers? Empower your organization by finding the right volunteers to support your mission. Streamline your recruitment process, manage applications efficiently, and maximize your impact by connecting with skilled and dedicated volunteers.
+             </p>
           </div>
           <Link to="/Organization">
             <button className="bg-blue text-cream py-2 px-10 rounded-full font-semibold hover:bg-yellow-dark transition duration-300">
@@ -45,7 +48,7 @@ const Register1 = () => {
         </div>
         {/* Right side: Image */}
         <div className="w-[370px] flex items-center justify-center">
-          <img src={placeholderImage} alt="Placeholder" className="h-64 w-72 object-cover rounded-lg shadow-lg border-2 border-blue-500" />
+          <img src="https://www.amnesty.org/en/wp-content/uploads/2022/04/GettyImages-97004996.jpg" alt="Placeholder" className="h-64 w-72 object-cover rounded-lg shadow-lg border-2 border-blue-500" />
         </div>
       </div>
       <div className='h-28'></div>
